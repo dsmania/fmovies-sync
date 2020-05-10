6f999964ac260946d4e3420ac714903eea08d0d0
dsmania <dsmania@hotmail.com>
2020-05-10 23:48:25 +0200

    Fixing CORS

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+      res.header('Access-Control-Max-Age', 120);
+      return res.status(200).json({});
+  }
+  next();
+});
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 server.origins("*:*");
