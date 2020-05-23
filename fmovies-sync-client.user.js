a3b5876b501b8d4859a3fcc3e027dc0fce0aa39c
dsmania <dsmania@kilois.org>
2020-05-23 07:15:10 +0200

    v0.1.2 RC

--- fmovies-sync-client.user.js
+++ fmovies-sync-client.user.js
@@ -126,188 +126,114 @@
         socket.emit('leave', id);
     }
 
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://use.fontawesome.com/releases/v5.13.0/css/all.css';
-    link.crossorigin = 'anonymous';
-    document.getElementsByTagName('head')[0].appendChild(link);
-
-    var syncButton = document.createElement('DIV');
-    syncButton.style = 'width: 44px; height: 44px; cursor: pointer; align-items: center; display: flex; justify-content: center; background-color: #0000';
-    var syncButtonText = document.createElement('DIV');
-    syncButtonText.style = 'width: 20px; height: 16px; color: #fffc; background-color: #0000; border: 2px solid #fffc; text-align: center; line-height: 11px; font-family: \'Font Awesome 5 Free\'; font-size: 9px; font-weight: 900; mix-blend-mode: normal;';
-    syncButtonText.innerHTML = '';
-    syncButton.appendChild(syncButtonText);
-    var syncButtonTooltip = document.createElement('DIV');
+    var syncButton = document.createElement('div');
+    syncButton.style = 'width: 44px; height: 44px; cursor: pointer; align-items: center; display: flex; justify-content: center; background-color: #0000; opacity: 80%;';
+    var syncButtonIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    syncButtonIcon.setAttribute('viewBox', '0 0 25 20');
+    syncButtonIcon.setAttribute('width', '25');
+    syncButtonIcon.setAttribute('height', '20');
+    var syncButtonIconBackground = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    syncButtonIconBackground.setAttribute('x', '1.5');
+    syncButtonIconBackground.setAttribute('y', '1.5');
+    syncButtonIconBackground.setAttribute('width', '22');
+    syncButtonIconBackground.setAttribute('height', '17');
+    syncButtonIconBackground.setAttribute('fill', '#fff0');
+    syncButtonIconBackground.setAttribute('stroke', '#fff');
+    syncButtonIconBackground.setAttribute('stroke-width', '3');
+    syncButtonIcon.appendChild(syncButtonIconBackground);
+    var syncButtonIconGlyph = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    syncButtonIconGlyph.innerHTML = '⇆';
+    syncButtonIconGlyph.setAttribute('font-family', 'Arial, Helvetica, sans-serif');
+    syncButtonIconGlyph.setAttribute('font-size', '15');
+    syncButtonIconGlyph.setAttribute('x', '6');
+    syncButtonIconGlyph.setAttribute('y', '14');
+    syncButtonIconGlyph.setAttribute('fill', '#fff');
+    syncButtonIcon.appendChild(syncButtonIconGlyph);
+    var syncButtonIconMask = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
+    syncButtonIconMask.setAttribute('id', 'glyphMask');
+    var syncButtonIconMaskBackground = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    syncButtonIconMaskBackground.setAttribute('x', '1.5');
+    syncButtonIconMaskBackground.setAttribute('y', '1.5');
+    syncButtonIconMaskBackground.setAttribute('width', '22');
+    syncButtonIconMaskBackground.setAttribute('height', '17');
+    syncButtonIconMaskBackground.setAttribute('fill', '#fff');
+    syncButtonIconMaskBackground.setAttribute('stroke', '#fff');
+    syncButtonIconMaskBackground.setAttribute('stroke-width', '3');
+    syncButtonIconMask.appendChild(syncButtonIconMaskBackground);
+    var syncButtonIconMaskGlyph = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    syncButtonIconMaskGlyph.innerHTML = '⇆';
+    syncButtonIconMaskGlyph.setAttribute('font-family', 'Arial, Helvetica, sans-serif');
+    syncButtonIconMaskGlyph.setAttribute('font-size', '15');
+    syncButtonIconMaskGlyph.setAttribute('x', '6');
+    syncButtonIconMaskGlyph.setAttribute('y', '14');
+    syncButtonIconMaskGlyph.setAttribute('fill', '#000');
+    syncButtonIconMask.appendChild(syncButtonIconMaskGlyph);
+    syncButtonIcon.appendChild(syncButtonIconMask);
+    syncButton.appendChild(syncButtonIcon);
+    var syncButtonTooltip = document.createElement('div');
     syncButtonTooltip.style = 'position: absolute; bottom: 68px; transition: 100ms cubic-bezier(0, .25, .25, 1) 500ms; transform: translate(0, 50%); visibility: hidden;';
-    var syncButtonTooltipText = document.createElement('DIV');
+    var syncButtonTooltipText = document.createElement('div');
     syncButtonTooltipText.innerHTML = 'Synchronize';
     syncButtonTooltipText.style = 'color: #000; background-color: #fff; font-size: 9px; border-radius: 1px; padding: 4px 10px;';
     syncButtonTooltip.appendChild(syncButtonTooltipText);
-    var syncButtonTooltipPointer = document.createElement('DIV');
+    var syncButtonTooltipPointer = document.createElement('div');
     syncButtonTooltipPointer.style = 'display: block; box-sizing: border-box; position: absolute; top: 100%; left: 50%; width: 10px; height: 9px; border-radius: 1px; transform: translate(-50%, -50%) rotate(45deg); z-index: -1; background-color: #fff;';
     syncButtonTooltip.appendChild(syncButtonTooltipPointer);
     syncButton.appendChild(syncButtonTooltip);
     syncButton.onmouseover = (e) => {
-        if (synced) {
-            // TODO
-            syncButtonText.style.color = '#000';
-            syncButtonText.style.borderColor = '#fff';
-            syncButtonText.style.backgroundColor = '#fff';
-            syncButtonText.style.mixBlendMode = 'lighten';
-        } else {
-            syncButtonText.style.color = '#fff';
-            syncButtonText.style.borderColor = '#fff';
-            syncButtonText.style.backgroundColor = '#0000';
-            syncButtonText.style.mixBlendMode = 'normal';
-        }
+        syncButton.style.opacity = '100%';
         syncButtonTooltip.style.transform = 'none';
         syncButtonTooltip.style.visibility = 'visible';
         syncButtonTooltip.style.transition = '100ms cubic-bezier(0, .25, .25, 1) 500ms';
     };
     syncButton.onmouseout = (e) => {
-        if (synced) {
-            // TODO
-            syncButtonText.style.color = '#000c';
-            syncButtonText.style.borderColor = '#fff0';
-            syncButtonText.style.backgroundColor = '#fffc';
-            syncButtonText.style.mixBlendMode = 'lighten';
-        } else {
-            syncButtonText.style.color = '#fffc';
-            syncButtonText.style.borderColor = '#fffc';
-            syncButtonText.style.backgroundColor = '#0000';
-            syncButtonText.style.mixBlendMode = 'normal';
-        }
+        syncButton.style.opacity = '80%';
         syncButtonTooltip.style.transform = 'translate(0, 50%)';
         syncButtonTooltip.style.visibility = 'hidden';
         syncButtonTooltip.style.transition = '100ms cubic-bezier(0, .25, .25, 1) 0ms';
     };
     syncButton.onclick = (e) => {
         if (synced) {
-            syncButtonText.style.color = '#fff';
-            syncButtonText.style.borderColor = '#fff';
-            syncButtonText.style.backgroundColor = '#0000';
-            syncButtonText.style.mixBlendMode = 'normal';
+            syncButtonIconGlyph.setAttribute('visibility', 'visible');
+            syncButtonIconBackground.setAttribute('fill', '#fff0');
+            syncButtonIconBackground.setAttribute('mask', null);
             syncButtonTooltipText.innerHTML = 'Synchronize';
             unsync();
         } else {
-            // TODO Copy from onmouseover
-            syncButtonText.style.color = '#000';
-            syncButtonText.style.borderColor = '#fff';
-            syncButtonText.style.backgroundColor = '#fff';
-            syncButtonText.style.mixBlendMode = 'lighten';
+            syncButtonIconGlyph.setAttribute('visibility', 'hidden');
+            syncButtonIconBackground.setAttribute('fill', '#fff');
+            syncButtonIconBackground.setAttribute('mask', 'url(#glyphMask)');
             syncButtonTooltipText.innerHTML = 'Synchronized (' + participants + ')';
             sync();
         }
-    };
-
-    var stretchCombo = document.createElement('DIV');
-    stretchCombo.style = 'height: 44px; align-items: center; display: flex; justify-content: center; color: fffc;';
-    var stretchComboSelect = document.createElement('SELECT');
-    stretchComboSelect.style = 'width: 20px; height: 16px; margin: 12px; cursor: pointer; font-family: \'Font Awesome 5 Free\'; font-size: 11px; font-weight: 900; text-align-last: center; background-color: #fff; color: #000; border: 0px none; -webkit-appearance: none; -moz-appearance: none; appearance: none; outline: none !important;';
-    var stretchComboSelectOption = document.createElement('OPTION');
-    stretchComboSelectOption.value = 'uniform';
-    stretchComboSelectOption.title = 'Fit';
-    stretchComboSelectOption.innerHTML = '';
-    stretchComboSelect.appendChild(stretchComboSelectOption);
-    stretchComboSelectOption = document.createElement('OPTION');
-    stretchComboSelectOption.value = 'fill';
-    stretchComboSelectOption.title = 'Fill';
-    stretchComboSelectOption.innerHTML = '';
-    stretchComboSelect.appendChild(stretchComboSelectOption);
-    stretchComboSelectOption = document.createElement('OPTION');
-    stretchComboSelectOption.value = 'exactfit';
-    stretchComboSelectOption.title = 'Stretch';
-    stretchComboSelectOption.innerHTML = '';
-    stretchComboSelect.appendChild(stretchComboSelectOption);
-    stretchComboSelectOption = document.createElement('OPTION');
-    stretchComboSelectOption.value = 'none';
-    stretchComboSelectOption.title = 'Center';
-    stretchComboSelectOption.innerHTML = '';
-    stretchComboSelect.appendChild(stretchComboSelectOption);
-    stretchComboSelect.selectedIndex = [ 'uniform', 'fill', 'exactfit', 'none' ].indexOf(player.getStretching());
-    stretchComboSelect.onchange = (e) => {
-        player.setConfig({ stretching: stretchComboSelect.value });
-    };
-    stretchCombo.appendChild(stretchComboSelect);
-    var stretchComboTooltip = document.createElement('DIV');
-    stretchComboTooltip.style = 'position: absolute; bottom: 68px; transition: 100ms cubic-bezier(0, .25, .25, 1) 500ms; transform: translate(0, 50%); visibility: hidden;';
-    var stretchComboTooltipText = document.createElement('DIV');
-    stretchComboTooltipText.innerHTML = 'Image adjust';
-    stretchComboTooltipText.style = 'color: #000; background-color: #fff; font-size: 9px; border-radius: 1px; padding: 4px 10px;';
-    stretchComboTooltip.appendChild(stretchComboTooltipText);
-    var stretchComboTooltipPointer = document.createElement('DIV');
-    stretchComboTooltipPointer.style = 'display: block; box-sizing: border-box; position: absolute; top: 100%; left: 50%; width: 10px; height: 9px; border-radius: 1px; transform: translate(-50%, -50%) rotate(45deg); z-index: -1; background-color: #fff;';
-    stretchComboTooltip.appendChild(stretchComboTooltipPointer);
-    stretchCombo.appendChild(stretchComboTooltip);
-    stretchCombo.onmouseover = (e) => {
-        stretchCombo.style.color = '#fff';
-        stretchComboTooltip.style.transform = 'none';
-        stretchComboTooltip.style.visibility = 'visible';
-        stretchComboTooltip.style.transition = '100ms cubic-bezier(0, .25, .25, 1) 500ms';
-    };
-    stretchCombo.onmouseout = (e) => {
-        stretchCombo.style.color = '#fffc';
-        stretchComboTooltip.style.transform = 'translate(0, 50%)';
-        stretchComboTooltip.style.visibility = 'hidden';
-        stretchComboTooltip.style.transition = '100ms cubic-bezier(0, .25, .25, 1) 0ms';
     };
 
     var buttonContainer = document.getElementsByClassName('jw-button-container')[0];
     function layoutUi() {
         var ccButton = buttonContainer.getElementsByClassName('jw-icon-cc')[0];
         buttonContainer.insertBefore(syncButton, ccButton);
-        buttonContainer.insertBefore(stretchCombo, syncButton);
 
         function fixButtonsSize() {
             if (ccButton.clientHeight < 60) {
                 syncButton.style.width = '44px';
                 syncButton.style.height = '44px';
-                syncButtonText.style.width = '20px';
-                syncButtonText.style.height = '16px';
-                syncButtonText.style.borderWidth = '2px';
-                syncButtonText.style.lineHeight = '11px';
-                syncButtonText.style.fontSize = '9px';
+                syncButtonIcon.setAttribute('width', '20px');
+                syncButtonIcon.setAttribute('height', '16px');
                 syncButtonTooltip.style.bottom = '68px';
                 syncButtonTooltipText.style.fontSize = '9px';
                 syncButtonTooltipText.style.padding = '4px 10px';
                 syncButtonTooltipPointer.style.width = '10px';
                 syncButtonTooltipPointer.style.height = '9px';
-
-                stretchCombo.style.height = '44px;';
-                stretchComboSelect.style.width = '20px';
-                stretchComboSelect.style.height = '16px';
-                stretchComboSelect.style.margin = '12px';
-                stretchComboSelect.style.fontSize = '11px';
-                stretchComboTooltip.style.bottom = '68px';
-                stretchComboTooltipText.style.fontSize = '9px';
-                stretchComboTooltipText.style.padding = '4px 10px';
-                stretchComboTooltipPointer.style.width = '10px';
-                stretchComboTooltipPointer.style.height = '9px';
             } else {
                 syncButton.style.width = '60px';
                 syncButton.style.height = '60px';
-                syncButtonText.style.width = '25px';
-                syncButtonText.style.height = '20px';
-                syncButtonText.style.borderWidth = '3px';
-                syncButtonText.style.lineHeight = '13px';
-                syncButtonText.style.fontSize = '12px';
+                syncButtonIcon.setAttribute('width', '25px');
+                syncButtonIcon.setAttribute('height', '20px');
                 syncButtonTooltip.style.bottom = '83px';
                 syncButtonTooltipText.style.fontSize = '16px';
                 syncButtonTooltipText.style.padding = '7px 10px';
                 syncButtonTooltipPointer.style.width = '11px';
                 syncButtonTooltipPointer.style.height = '10px';
-
-                stretchCombo.style.height = '60px;';
-                stretchComboSelect.style.width = '25px';
-                stretchComboSelect.style.height = '20px';
-                stretchComboSelect.style.margin = '20px';
-                stretchComboSelect.style.fontSize = '13px';
-                stretchComboTooltip.style.bottom = '83px';
-                stretchComboTooltipText.style.fontSize = '16px';
-                stretchComboTooltipText.style.padding = '7px 10px';
-                stretchComboTooltipPointer.style.width = '11px';
-                stretchComboTooltipPointer.style.height = '10px';
             }
         };
         fixButtonsSize();
